d63d3b38ddb9cc0e459af18ac95d048938a83bcf
SRYANG <sarang628@naver.com>
2023-05-03 15:30:30 +0800

    update

--- app/src/main/java/com/sryang/myreview/MainActivity.kt
+++ app/src/main/java/com/sryang/myreview/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.sryang.myreview
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sryang.myreview.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
--- library/src/main/java/com/sarang/myreview/ui/AddReviewFragment.kt
+++ library/src/main/java/com/sarang/myreview/ui/AddReviewFragment.kt
@@ -7,17 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.sarang.myreview.databinding.FragmentAddReview1Binding
 import com.sarang.myreview.ui.adapter.UploadedPictureAdapter
 import com.sarang.myreview.ui.usecase.AddReviewFragmentLayoutUseCase
-import com.sarang.myreview.ui.viewmodel.MyReviewViewModel
-import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 /**
  * layout - [FragmentAddReview1Binding]
@@ -26,13 +21,8 @@ import javax.inject.Inject
  * [UploadedPictureAdapter]
  * [MyReviewViewModel]
  */
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class MyReviewFragment : Fragment() {
-    @Inject
-    lateinit var findRestaurantNavigation: FindRestaurantNavigation
-
-    /* 내리뷰 뷰모델 */
-    private val mViewModel: MyReviewViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -43,26 +33,8 @@ class MyReviewFragment : Fragment() {
             .apply {
                 lifecycleOwner = viewLifecycleOwner
             }
-        getReviewId()?.let { mViewModel.loadReview(it) } // 리뷰 불러오기
-        getRestaurantId()?.let { mViewModel.selectRestaurant(it) } // 식당 불러오기
         return binding.root
     }
-
-    private fun MyReviewViewModel.subScribeUiState(
-        useCase: MutableStateFlow<AddReviewFragmentLayoutUseCase>
-    ) {
-        viewLifecycleOwner.lifecycleScope.launch {
-            uiState.collect { myReivewUiState ->
-                useCase.update {
-                    it.copy(
-                        pictureAdapterUseCase = it.pictureAdapterUseCase.copy(images = myReivewUiState.selectedImagePath)
-                    )
-                }
-                //update layoutUsecase
-            }
-        }
-    }
-
     private fun FragmentAddReview1Binding.subScribeLayoutUseCase(
         useCase: MutableStateFlow<AddReviewFragmentLayoutUseCase>
     ) {
